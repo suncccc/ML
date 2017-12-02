0f3668a4b35ef4327aa8349b4fa6aa4347f4bcf9
suncccc <suncccc@gmail.com>
2017-12-02 16:37:01 -0600

    Initial commit

---
+++ Scikit Learn.py
@@ -0,0 +1,267 @@
+from sklearn import datasets
+
+digits = datasets.load_digits()
+
+print(digits)
+
+
+import pandas as pd
+
+digits = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/optdigits/optdigits.tra", header=None)
+print(digits)
+
+digits.to_csv('digits.csv')
+
+print(digits.keys())
+print(digits.DESCR)
+
+print(digits.target)
+
+digits_data = digits.images
+print(digits_data.shape)
+
+# Import matplotlib
+import matplotlib.pyplot as plt
+
+# Figure size (width, height) in inches
+fig = plt.figure(figsize=(6, 6))
+
+# Adjust the subplots
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+
+# For each of the 64 images
+for i in range(64):
+    # Initialize the subplots: add a subplot in the grid of 8 by 8, at the i+1-th position
+    ax = fig.add_subplot(8, 8, i + 1, xticks=[], yticks=[])
+    # Display an image at the i-th position
+    ax.imshow(digits.images[i], cmap=plt.cm.binary, interpolation='nearest')
+    # label the image with the target value
+    ax.text(0, 7, str(digits.target[i]))
+
+# Show the plot
+plt.show()
+
+# Import matplotlib
+import matplotlib.pyplot as plt
+
+# Join the images and target labels in a list
+images_and_labels = list(zip(digits.images, digits.target))
+
+# for every element in the list
+for index, (image, label) in enumerate(images_and_labels[:8]):
+    # initialize a subplot of 2X4 at the i+1-th position
+    plt.subplot(2, 4, index + 1)
+    # Don't plot any axes
+    plt.axis('off')
+    # Display images in all subplots
+    plt.imshow(image, cmap=plt.cm.gray_r,interpolation='nearest')
+    # Add a title to each subplot
+    plt.title('Training: ' + str(label))
+
+# Show the plot
+plt.show()
+
+
+from sklearn.decomposition import *
+
+# Create a Randomized PCA model that takes two components
+randomized_pca = RandomizedPCA(n_components=2)
+
+# Fit and transform the data to the model
+reduced_data_rpca = randomized_pca.fit_transform(digits.data)
+
+# Create a regular PCA model
+pca = PCA(n_components=2)
+
+# Fit and transform the data to the model
+reduced_data_pca = pca.fit_transform(digits.data)
+
+# Inspect the shape
+reduced_data_pca.shape
+
+# Print out the data
+print(reduced_data_rpca)
+print(reduced_data_pca)
+
+colors = ['black', 'blue', 'purple', 'yellow', 'white', 'red', 'lime', 'cyan', 'orange', 'gray']
+for i in range(len(colors)):
+    x = reduced_data_rpca[:, 0][digits.target == i]
+    y = reduced_data_rpca[:, 1][digits.target == i]
+    plt.scatter(x, y, c=colors[i])
+plt.legend(digits.target_names, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.xlabel('First Principal Component')
+plt.ylabel('Second Principal Component')
+plt.title("PCA Scatter Plot")
+plt.show()
+
+from sklearn.preprocessing import scale
+
+data = scale(digits.data)
+
+# Import `train_test_split`
+from sklearn.cross_validation import train_test_split
+# Split the `digits` data into training and test sets
+X_train, X_test, y_train, y_test, images_train, images_test = train_test_split(data, digits.target, digits.images, test_size=0.25, random_state=42)
+
+clf = cluster.KMeans(init='k-means++',n_clusters = 10, random_state=42)
+clf.fit(X_train)
+
+
+
+# Figure size in inches
+fig = plt.figure(figsize=(8, 3))
+
+# Add title
+fig.suptitle('Cluster Center Images', fontsize=14, fontweight='bold')
+
+# For all labels (0-9)
+for i in range(10):
+    # Initialize subplots in a grid of 2X5, at i+1th position
+    ax = fig.add_subplot(2, 5, 1 + i)
+    # Display images
+    ax.imshow(clf.cluster_centers_[i].reshape((8, 8)), cmap=plt.cm.binary)
+    # Don't show the axes
+    plt.axis('off')
+
+# Show the plot
+plt.show()
+
+# Predict the labels for `X_test`
+y_pred=clf.predict(X_test)
+
+# Print out the first 100 instances of `y_pred`
+print(y_pred[:100])
+
+# Print out the first 100 instances of `y_test`
+print(y_test[:100])
+
+# Study the shape of the cluster centers
+clf.cluster_centers_.shape
+
+
+# Import `Isomap()`
+from sklearn.manifold import Isomap
+
+# Create an isomap and fit the `digits` data to it
+X_iso = Isomap(n_neighbors=10).fit_transform(X_train)
+
+# Compute cluster centers and predict cluster index for each sample
+clusters = clf.fit_predict(X_train)
+
+# Create a plot with subplots in a grid of 1X2
+fig, ax = plt.subplots(1, 2, figsize=(8, 4))
+
+# Adjust layout
+fig.suptitle('Predicted Versus Training Labels', fontsize=14, fontweight='bold')
+fig.subplots_adjust(top=0.85)
+
+# Add scatterplots to the subplots
+ax[0].scatter(X_iso[:, 0], X_iso[:, 1], c=clusters)
+ax[0].set_title('Predicted Training Labels')
+ax[1].scatter(X_iso[:, 0], X_iso[:, 1], c=y_train)
+ax[1].set_title('Actual Training Labels')
+
+# Show the plots
+plt.show()
+
+print(metrics.confusion_matrix(y_test, y_pred))
+
+#--------------------SVM
+
+# Import the `svm` model
+from sklearn import svm
+
+# Create the SVC model
+svc_model = svm.SVC(gamma=0.001, C=100., kernel='linear')
+
+# Fit the data to the SVC model
+svc_model.fit(X_train, y_train)
+
+# Split the `digits` data into two equal sets
+X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.5, random_state=0)
+
+# Import GridSearchCV
+from sklearn.grid_search import GridSearchCV
+
+# Set the parameter candidates
+parameter_candidates = [
+  {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+  {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+]
+
+# Create a classifier with the parameter candidates
+clf = GridSearchCV(estimator=svm.SVC(), param_grid=parameter_candidates, n_jobs=-1)
+
+# Train the classifier on training data
+clf.fit(X_train, y_train)
+
+# Print out the results
+
+print('Best score for training data:', clf.best_score_)
+print('Best `C`:',clf.best_estimator_.C)
+print('Best kernel:',clf.best_estimator_.kernel)
+print('Best `gamma`:',clf.best_estimator_.gamma)
+
+# Apply the classifier to the test data, and view the accuracy score
+clf.score(X_test, y_test)
+
+# Train and score a new classifier with the grid search parameters
+svm.SVC(C=10, kernel='rbf', gamma=0.001).fit(X_train, y_train).score(X_test, y_test)
+
+y_pred = svc_model.predict(X_test)
+print(y_pred)
+print(y_test)
+
+print(metrics.confusion_matrix(y_test, y_pred))
+
+
+# Import matplotlib
+import matplotlib.pyplot as plt
+
+# Assign the predicted values to `predicted`
+predicted = svc_model.predict(X_test)
+
+# Zip together the `images_test` and `predicted` values in `images_and_predictions`
+images_and_predictions = list(zip(images_test, predicted))
+
+# For the first 4 elements in `images_and_predictions`
+for index, (image, prediction) in enumerate(images_and_predictions[:4]):
+    # Initialize subplots in a grid of 1 by 4 at positions i+1
+    plt.subplot(1, 4, index + 1)
+    # Don't show axes
+    plt.axis('off')
+    # Display images in all subplots in the grid
+    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    # Add a title to the plot
+    plt.title('Predicted: ' + str(prediction))
+
+# Show the plot
+plt.show()
+
+# Import `Isomap()`
+from sklearn.manifold import Isomap
+
+# Create an isomap and fit the `digits` data to it
+X_iso = Isomap(n_neighbors=10).fit_transform(X_train)
+
+# Compute cluster centers and predict cluster index for each sample
+predicted = svc_model.predict(X_train)
+
+# Create a plot with subplots in a grid of 1X2
+fig, ax = plt.subplots(1, 2, figsize=(8, 4))
+
+# Adjust the layout
+fig.subplots_adjust(top=0.85)
+
+# Add scatterplots to the subplots
+ax[0].scatter(X_iso[:, 0], X_iso[:, 1], c=predicted)
+ax[0].set_title('Predicted labels')
+ax[1].scatter(X_iso[:, 0], X_iso[:, 1], c=y_train)
+ax[1].set_title('Actual Labels')
+
+
+# Add title
+fig.suptitle('Predicted versus actual labels', fontsize=14, fontweight='bold')
+
+# Show the plot
+plt.show()
